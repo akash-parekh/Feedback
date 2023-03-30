db081d2573ba13e91cac636d59c69d45aa833345
Akash <akashparekh98@gmail.com>
2023-03-30 09:07:38 +0530

    Created db for form and user formModel to generate the form

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
 
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
-        print(entered_username)
-        return HttpResponseRedirect("/thank_you")
-    return render(request, "reviews/review.html")
+        form = ReviewForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank_you")
+    
+    else:
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html",{
+        "form":form
+    })
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
