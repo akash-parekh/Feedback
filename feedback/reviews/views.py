c2bbe03bd7a3293d740464c0256eb68801c9f77c
Akash <akashparekh98@gmail.com>
2023-03-30 13:25:12 +0530

    Added ListView, CreateView and DetailView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,28 +2,40 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        exsisting_data = Review.objects.get(id = 1)
-        form = ReviewForm(instance=exsisting_data)
-        return render(request, "reviews/review.html",{
-            "form":form
-        })
+class ReviewView(CreateView):
+#     def get(self, request):
+#         exsisting_data = Review.objects.get(id = 1)
+#         form = ReviewForm(instance=exsisting_data)
+#         return render(request, "reviews/review.html",{
+#             "form":form
+#         })
+    form_class = ReviewForm
+    model = Review
+    # fields = "__all__"
+    template_name = "reviews/review.html"
+    success_url = "/thank_you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank_you")
-        return render(request, "reviews/review.html",{
-            "form":form
-        })
+    # def form_valid(self, form):
+    #     form.save()
+    #     return super().form_valid(form)
+    
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():   Validation handeled by FormView Class
+#             form.save()
+#             return HttpResponseRedirect("/thank_you")
+#         return render(request, "reviews/review.html",{
+#             "form":form
+#         })
+
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
@@ -33,22 +45,13 @@ class ThankYouView(TemplateView):
         context['message'] = "This Works!"
         return context
     
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
 
-    def get_context_data(self, **kwargs) :
-        context =  super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context['reviews'] = reviews
-        return context
     
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = 'reviews/single_review.html'
-
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        reviews = Review.objects.get(pk = review_id)
-        context['review'] = reviews
-        return context
+    model = Review
     
