96d57610d2a6b560d231ccb31e5748ec87e053a0
Akash <akashparekh98@gmail.com>
2023-03-30 09:45:37 +0530

    added model to admin page and created a label for forms using existing data. Used generic class based view.

--- feedback/reviews/models.py
+++ feedback/reviews/models.py
@@ -6,3 +6,6 @@ class Review(models.Model):
     user_name = models.CharField(max_length=200)
     review_text = models.TextField()
     rating = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.id} {self.user_name}"
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
+
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        exsisting_data = Review.objects.get(id = 1)
+        form = ReviewForm(instance=exsisting_data)
+        return render(request, "reviews/review.html",{
+            "form":form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
-        
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank_you")
-    
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html",{
-        "form":form
-    })
+        return render(request, "reviews/review.html",{
+            "form":form
+        })
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
