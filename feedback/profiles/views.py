21626f4bd57501ce6285527922445e43e7f899d2
Akash <akashparekh98@gmail.com>
2023-04-04 16:09:27 +0530

    Added model for image upload

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -7,12 +7,6 @@ from .models import UserProfile
 
 # Create your views here.
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
-
-
 class CreateProfileView(View):
     def get(self, request):
         form = ProfileForm()
@@ -24,6 +18,7 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
         if submitted_form.is_valid():
             profile = UserProfile(image = request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
         
         return render(request, "profiles/create_profile.html", {
